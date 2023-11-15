ddc5ea63b476f5f620d97c67c0dc4826b4189932
uikoo9 <uikoo9@qq.com>
2023-11-15 10:47:27 +0800

    feat(qiao-get-ip): modify github url

--- packages/qiao-get-ip/test.js
+++ packages/qiao-get-ip/test.js
@@ -9,21 +9,21 @@ const { getIP } = require('./index.js');
 
 // nodejs
 test.serial('get ip on nodejs', async (t) => {
-  t.timeout(900);
+  t.timeout(2000);
 
-  const ip = await getIP(900);
+  const ip = await getIP(2000);
   t.log(ip);
   t.truthy(ip);
 });
 
 // browser
 test.serial('get ip on browser', async (t) => {
-  t.timeout(900);
+  t.timeout(2000);
 
   const dom = new JSDOM('', { url: 'http://localhost' });
   global.document = dom.window.document;
 
-  const ip = await getIP(900);
+  const ip = await getIP(2000);
   t.log(ip);
   t.truthy(ip);
 });
