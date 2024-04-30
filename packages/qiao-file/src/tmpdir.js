928944115afe0ac8900fe72875e5f55037416685
uikoo9 <uikoo9@qq.com>
2024-04-30 21:18:18 +0800

    feat(qiao-nodejs): add tmpdir

---
+++ packages/qiao-file/src/tmpdir.js
@@ -0,0 +1,37 @@
+// os
+import os from 'os';
+
+// path
+import path from 'path';
+
+// uuid
+import { uuid } from 'qiao-encode';
+
+// fs
+import { ensureDir } from 'fs-extra';
+
+// debug
+import Debug from 'debug';
+const debug = Debug('qiao-file');
+
+/**
+ * mk dir
+ * @param {*} dir
+ * @returns
+ */
+export const tmpdir = async () => {
+  try {
+    const tmpPath = os.tmpdir();
+    const randomFolder = uuid();
+    const tmpDir = path.resolve(tmpPath, randomFolder);
+    debug('/ tmpdir / ', tmpDir);
+
+    await ensureDir(tmpDir);
+    debug('/ tmpdir / success');
+
+    return true;
+  } catch (e) {
+    debug('/ tmpdir / fail');
+    console.log(e);
+  }
+};
