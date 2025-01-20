2f8c5d7561a77af4752b55efa416cf2984f82b2e
uikoo9 <uikoo9@qq.com>
2025-01-20 17:39:59 +0800

    feat(qiao-talker): add emo talker

--- packages/qiao-talker/index.js
+++ packages/qiao-talker/index.js
@@ -4,15 +4,200 @@ var qiaoAjax = require('qiao-ajax');
 var qiao_log_js = require('qiao.log.js');
 
 // qiao
-const logger = qiao_log_js.Logger('qiao-talker');
+const logger$1 = qiao_log_js.Logger('qiao-talker');
 
 /**
- * checkFace
+ * emoCheckFace
  * @param {*} options
  * @returns
  */
-const checkFace = async (options) => {
-  const methodName = 'checkFace';
+const emoCheckFace = async (options) => {
+  const methodName = 'emoCheckFace';
+
+  // const
+  const appKey = options.appKey;
+  const imageUrl = options.imageUrl;
+  logger$1.info(methodName, 'appKey', appKey);
+  logger$1.info(methodName, 'imageUrl', imageUrl);
+
+  // check
+  if (!appKey) {
+    logger$1.info(methodName, 'need appKey');
+    return;
+  }
+  if (!imageUrl) {
+    logger$1.info(methodName, 'need imageUrl');
+    return;
+  }
+
+  // req
+  try {
+    const url = 'https://dashscope.aliyuncs.com/api/v1/services/aigc/image2video/face-detect';
+    const res = await qiaoAjax.post(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${appKey}`,
+      },
+      data: {
+        model: 'emo-detect-v1',
+        input: {
+          image_url: imageUrl,
+        },
+      },
+    });
+
+    // check
+    if (res.status !== 200) {
+      logger$1.error(methodName, `status is ${res.status}`);
+      return;
+    }
+    if (res.data && res.data.code) {
+      logger$1.error(methodName, 'request failed', res.data);
+      return;
+    }
+
+    // r
+    return res.data;
+  } catch (error) {
+    logger$1.error(methodName, 'request error', error);
+  }
+};
+
+/**
+ * emoStartTalker
+ * @param {*} options
+ * @returns
+ */
+const emoStartTalker = async (options) => {
+  const methodName = 'emoStartTalker';
+
+  // const
+  const appKey = options.appKey;
+  const imageUrl = options.imageUrl;
+  const audioUrl = options.audioUrl;
+  const face_bbox = options.face_bbox;
+  const ext_bbox = options.ext_bbox;
+  logger$1.info(methodName, 'appKey', appKey);
+  logger$1.info(methodName, 'imageUrl', imageUrl);
+  logger$1.info(methodName, 'audioUrl', audioUrl);
+  logger$1.info(methodName, 'face_bbox', face_bbox);
+  logger$1.info(methodName, 'ext_bbox', ext_bbox);
+
+  // check
+  if (!appKey) {
+    logger$1.info(methodName, 'need appKey');
+    return;
+  }
+  if (!imageUrl) {
+    logger$1.info(methodName, 'need imageUrl');
+    return;
+  }
+  if (!audioUrl) {
+    logger$1.info(methodName, 'need audioUrl');
+    return;
+  }
+
+  // req
+  try {
+    const url = 'https://dashscope.aliyuncs.com/api/v1/services/aigc/image2video/video-synthesis/';
+    const res = await qiaoAjax.post(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${appKey}`,
+        'X-DashScope-Async': 'enable',
+      },
+      data: {
+        model: 'emo-v1',
+        input: {
+          image_url: imageUrl,
+          audio_url: audioUrl,
+          face_bbox: face_bbox,
+          ext_bbox: ext_bbox,
+        },
+        parameters: {
+          // 可选择动作风格强度控制人物的运动姿态和幅度，当前支持3种：normal、calm、active，分别对应人物动作风格适中、平静、活泼。默认为normal。
+          style_level: options.style_level || 'normal',
+        },
+      },
+    });
+
+    // check
+    if (res.status !== 200) {
+      logger$1.error(methodName, `status is ${res.status}`);
+      return;
+    }
+    if (res.data && res.data.code) {
+      logger$1.error(methodName, 'request failed', res.data);
+      return;
+    }
+
+    // r
+    return res.data;
+  } catch (error) {
+    logger$1.error(methodName, 'request error', error);
+  }
+};
+
+/**
+ * emoCheckTalker
+ * @param {*} options
+ * @returns
+ */
+const emoCheckTalker = async (options) => {
+  const methodName = 'emoCheckTalker';
+
+  // const
+  const appKey = options.appKey;
+  const taskId = options.taskId;
+  logger$1.info(methodName, 'appKey', appKey);
+  logger$1.info(methodName, 'taskId', taskId);
+
+  // check
+  if (!appKey) {
+    logger$1.info(methodName, 'need appKey');
+    return;
+  }
+  if (!taskId) {
+    logger$1.info(methodName, 'need taskId');
+    return;
+  }
+
+  // req
+  try {
+    const url = `https://dashscope.aliyuncs.com/api/v1/tasks/${taskId}`;
+    const res = await qiaoAjax.get(url, {
+      headers: {
+        Authorization: `Bearer ${appKey}`,
+      },
+    });
+
+    // check
+    if (res.status !== 200) {
+      logger$1.error(methodName, `status is ${res.status}`);
+      return;
+    }
+    if (res.data && res.data.code) {
+      logger$1.error(methodName, 'request failed', res.data);
+      return;
+    }
+
+    // r
+    return res.data;
+  } catch (error) {
+    logger$1.error(methodName, 'request error', error);
+  }
+};
+
+// qiao
+const logger = qiao_log_js.Logger('qiao-talker');
+
+/**
+ * liveCheckFace
+ * @param {*} options
+ * @returns
+ */
+const liveCheckFace = async (options) => {
+  const methodName = 'liveCheckFace';
 
   // const
   const appKey = options.appKey;
@@ -64,12 +249,12 @@ const checkFace = async (options) => {
 };
 
 /**
- * startTalker
+ * liveStartTalker
  * @param {*} options
  * @returns
  */
-const startTalker = async (options) => {
-  const methodName = 'startTalker';
+const liveStartTalker = async (options) => {
+  const methodName = 'liveStartTalker';
 
   // const
   const appKey = options.appKey;
@@ -148,12 +333,12 @@ const startTalker = async (options) => {
 };
 
 /**
- * checkTalker
+ * liveCheckTalker
  * @param {*} options
  * @returns
  */
-const checkTalker = async (options) => {
-  const methodName = 'checkTalker';
+const liveCheckTalker = async (options) => {
+  const methodName = 'liveCheckTalker';
 
   // const
   const appKey = options.appKey;
@@ -197,6 +382,9 @@ const checkTalker = async (options) => {
   }
 };
 
-exports.checkFace = checkFace;
-exports.checkTalker = checkTalker;
-exports.startTalker = startTalker;
+exports.emoCheckFace = emoCheckFace;
+exports.emoCheckTalker = emoCheckTalker;
+exports.emoStartTalker = emoStartTalker;
+exports.liveCheckFace = liveCheckFace;
+exports.liveCheckTalker = liveCheckTalker;
+exports.liveStartTalker = liveStartTalker;
---
+++ packages/qiao-talker/src/emo-talker.js
@@ -0,0 +1,188 @@
+// qiao
+import { get, post } from 'qiao-ajax';
+
+// Logger
+import { Logger } from 'qiao.log.js';
+const logger = Logger('qiao-talker');
+
+/**
+ * emoCheckFace
+ * @param {*} options
+ * @returns
+ */
+export const emoCheckFace = async (options) => {
+  const methodName = 'emoCheckFace';
+
+  // const
+  const appKey = options.appKey;
+  const imageUrl = options.imageUrl;
+  logger.info(methodName, 'appKey', appKey);
+  logger.info(methodName, 'imageUrl', imageUrl);
+
+  // check
+  if (!appKey) {
+    logger.info(methodName, 'need appKey');
+    return;
+  }
+  if (!imageUrl) {
+    logger.info(methodName, 'need imageUrl');
+    return;
+  }
+
+  // req
+  try {
+    const url = 'https://dashscope.aliyuncs.com/api/v1/services/aigc/image2video/face-detect';
+    const res = await post(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${appKey}`,
+      },
+      data: {
+        model: 'emo-detect-v1',
+        input: {
+          image_url: imageUrl,
+        },
+      },
+    });
+
+    // check
+    if (res.status !== 200) {
+      logger.error(methodName, `status is ${res.status}`);
+      return;
+    }
+    if (res.data && res.data.code) {
+      logger.error(methodName, 'request failed', res.data);
+      return;
+    }
+
+    // r
+    return res.data;
+  } catch (error) {
+    logger.error(methodName, 'request error', error);
+  }
+};
+
+/**
+ * emoStartTalker
+ * @param {*} options
+ * @returns
+ */
+export const emoStartTalker = async (options) => {
+  const methodName = 'emoStartTalker';
+
+  // const
+  const appKey = options.appKey;
+  const imageUrl = options.imageUrl;
+  const audioUrl = options.audioUrl;
+  const face_bbox = options.face_bbox;
+  const ext_bbox = options.ext_bbox;
+  logger.info(methodName, 'appKey', appKey);
+  logger.info(methodName, 'imageUrl', imageUrl);
+  logger.info(methodName, 'audioUrl', audioUrl);
+  logger.info(methodName, 'face_bbox', face_bbox);
+  logger.info(methodName, 'ext_bbox', ext_bbox);
+
+  // check
+  if (!appKey) {
+    logger.info(methodName, 'need appKey');
+    return;
+  }
+  if (!imageUrl) {
+    logger.info(methodName, 'need imageUrl');
+    return;
+  }
+  if (!audioUrl) {
+    logger.info(methodName, 'need audioUrl');
+    return;
+  }
+
+  // req
+  try {
+    const url = 'https://dashscope.aliyuncs.com/api/v1/services/aigc/image2video/video-synthesis/';
+    const res = await post(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${appKey}`,
+        'X-DashScope-Async': 'enable',
+      },
+      data: {
+        model: 'emo-v1',
+        input: {
+          image_url: imageUrl,
+          audio_url: audioUrl,
+          face_bbox: face_bbox,
+          ext_bbox: ext_bbox,
+        },
+        parameters: {
+          // 可选择动作风格强度控制人物的运动姿态和幅度，当前支持3种：normal、calm、active，分别对应人物动作风格适中、平静、活泼。默认为normal。
+          style_level: options.style_level || 'normal',
+        },
+      },
+    });
+
+    // check
+    if (res.status !== 200) {
+      logger.error(methodName, `status is ${res.status}`);
+      return;
+    }
+    if (res.data && res.data.code) {
+      logger.error(methodName, 'request failed', res.data);
+      return;
+    }
+
+    // r
+    return res.data;
+  } catch (error) {
+    logger.error(methodName, 'request error', error);
+  }
+};
+
+/**
+ * emoCheckTalker
+ * @param {*} options
+ * @returns
+ */
+export const emoCheckTalker = async (options) => {
+  const methodName = 'emoCheckTalker';
+
+  // const
+  const appKey = options.appKey;
+  const taskId = options.taskId;
+  logger.info(methodName, 'appKey', appKey);
+  logger.info(methodName, 'taskId', taskId);
+
+  // check
+  if (!appKey) {
+    logger.info(methodName, 'need appKey');
+    return;
+  }
+  if (!taskId) {
+    logger.info(methodName, 'need taskId');
+    return;
+  }
+
+  // req
+  try {
+    const url = `https://dashscope.aliyuncs.com/api/v1/tasks/${taskId}`;
+    const res = await get(url, {
+      headers: {
+        Authorization: `Bearer ${appKey}`,
+      },
+    });
+
+    // check
+    if (res.status !== 200) {
+      logger.error(methodName, `status is ${res.status}`);
+      return;
+    }
+    if (res.data && res.data.code) {
+      logger.error(methodName, 'request failed', res.data);
+      return;
+    }
+
+    // r
+    return res.data;
+  } catch (error) {
+    logger.error(methodName, 'request error', error);
+  }
+};
